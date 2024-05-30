08b2eeac8c95be95664ad2cfd790852cb1796733
beniusis <rokasbeniu@gmail.com>
2024-05-30 13:01:53 +0300

    impr: add slot element for Header

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,21 @@
 headerheader
 <script setup lang="ts">
-import { Header } from '@/components';
+import { Header, Navigation, SearchBar } from '@/components';
+import { ref } from 'vue';
+
+const searchValue = ref('');
 </script>
 
 <template>
   <div class="flex min-h-screen w-full flex-col bg-muted/40">
     <div class="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
-      <Header />
+      <Header>
+        <Navigation />
+        <h1 class="sr-only">literacite</h1>
+        <div class="relative ml-auto flex-1 md:grow-0">
+          <SearchBar :search-value="searchValue" @update="(newValue) => (searchValue = newValue)" />
+        </div>
+      </Header>
     </div>
   </div>
 </template>
