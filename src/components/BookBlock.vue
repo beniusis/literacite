7aa6db464222088384c3538c4e245b006a19390b
beniusis <rokasbeniu@gmail.com>
2024-06-11 19:03:26 +0300

    feat: create BookBlock component for a single book

---
+++ src/components/BookBlock.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import type { Book } from '@/lib/types';
+
+const props = defineProps<{
+  data: Book;
+}>();
+</script>
+
+<template>
+  <div class="row-span-2 grid cursor-pointer grid-rows-subgrid">
+    <div class="max-h-60 max-w-40">
+      <img
+        class="h-full w-full rounded-md object-cover"
+        :src="props.data.volumeInfo.imageLinks.thumbnail"
+        :alt="`Cover of ${props.data.volumeInfo.title}`"
+      />
+    </div>
+
+    <div class="text-balance">
+      <h2 class="font-headland text-sm text-primary">{{ props.data.volumeInfo.title }}</h2>
+      <p class="text-xs text-muted-foreground">{{ props.data.volumeInfo.authors.join(', ') }}</p>
+    </div>
+  </div>
+</template>
--- src/components/Content.vue
+++ src/components/Content.vue
@@ -4,6 +4,7 @@ import { TabsContent } from '@/components/ui/tabs';
 import EmptyPlaceholder from './EmptyPlaceholder.vue';
 import type { Book } from '@/lib/types';
 import type { ComputedRef } from 'vue';
+import BookBlock from './BookBlock.vue';
 
 const props = defineProps<{
   value: string;
@@ -24,25 +25,7 @@ const props = defineProps<{
     <Separator class="my-4 w-full" />
     <EmptyPlaceholder v-if="props.books.value.length === 0" />
     <div v-else class="grid grid-cols-auto gap-x-8 gap-y-4">
-      <div
-        v-for="(book, index) in books.value"
-        class="row-span-2 grid cursor-pointer grid-rows-subgrid"
-        :key="index"
-        :title="book.volumeInfo.title"
-      >
-        <div class="max-h-60 max-w-40">
-          <img
-            class="h-full w-full rounded-md object-cover"
-            :src="book.volumeInfo.imageLinks.thumbnail"
-            :alt="`Cover of ${book.volumeInfo.title}`"
-          />
-        </div>
-
-        <div class="text-balance">
-          <h2 class="font-headland text-sm text-primary">{{ book.volumeInfo.title }}</h2>
-          <p class="text-xs text-muted-foreground">{{ book.volumeInfo.authors.join(', ') }}</p>
-        </div>
-      </div>
+      <BookBlock v-for="(book, index) in books.value" :key="index" :data="book" />
     </div>
   </TabsContent>
 </template>
