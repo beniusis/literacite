6968a5d9fd5a9256bf7ce0df6af91f1d0060adda
beniusis <rokasbeniu@gmail.com>
2024-06-11 18:40:03 +0300

    feat: create types, create data.ts for constant values

---
+++ src/lib/types.ts
@@ -0,0 +1,30 @@
+type BookStatus = 'READ' | 'READING' | 'TO READ';
+
+export interface Book {
+  volumeInfo: {
+    title: string;
+    authors: string[];
+    publisher?: string;
+    publishedDate?: string;
+    description: string;
+    pageCount: number;
+    categories: string[];
+    averageRating?: number;
+    ratingsCount?: number;
+    imageLinks: {
+      smallThumbnail?: string;
+      thumbnail: string;
+    };
+    language: string;
+    canonicalVolumeLink?: string;
+  };
+  status?: BookStatus;
+}
+
+export type TabValue = 'all' | 'read' | 'reading' | 'to read';
+
+export interface Tab {
+  value: TabValue;
+  title: string;
+  subtitle: string;
+}
