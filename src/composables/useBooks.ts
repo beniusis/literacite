450e8d7ab6681a8496ca3cf214dc666c14872fd3
beniusis <rokasbeniu@gmail.com>
2024-06-12 16:40:52 +0300

    feat: create useBooks composable
    Used to retrieve books data from Google Books using its API.

---
+++ src/composables/useBooks.ts
@@ -0,0 +1,25 @@
+import { ref } from 'vue';
+import type { Book } from '@/lib/types';
+
+export const useBooks = async (query: string) => {
+  const loading = ref(false);
+  const error = ref(false);
+  const books = ref<Book[]>([]);
+
+  loading.value = true;
+  error.value = false;
+
+  try {
+    const response = await fetch(
+      `https://www.googleapis.com/books/v1/volumes?q=${query}&printType=books&maxResults=40&orderBy=relevance`
+    );
+    const data = await response.json();
+    books.value = data.items;
+  } catch {
+    error.value = true;
+  } finally {
+    loading.value = false;
+  }
+
+  return { loading, error, books };
+};
