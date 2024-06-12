563ce9d474a87a2a135d55335e401da11b78ce8d
beniusis <rokasbeniu@gmail.com>
2024-06-12 15:12:18 +0300

    feat: create Books component

---
+++ src/components/Books/Books.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import type { Book } from '@/lib/types';
+import BookBlock from './BookBlock.vue';
+
+const props = defineProps<{
+  data: Book[];
+}>();
+</script>
+
+<template>
+  <div class="grid grid-cols-auto gap-x-8 gap-y-4">
+    <BookBlock v-for="book in props.data" :key="book.id" :data="book" />
+  </div>
+</template>
