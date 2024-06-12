186e40a2d7c67f39f403b168f0a7c0bb05646885
beniusis <rokasbeniu@gmail.com>
2024-06-12 16:42:28 +0300

    chore: wrap the searchbar in a div with relative styling

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -12,13 +12,15 @@ const emit = defineEmits<{
 </script>
 
 <template>
-  <Search class="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-  <Input
-    id="search-input"
-    type="text"
-    placeholder="Search..."
-    class="w-full rounded-lg bg-background pe-16 pl-8 md:w-[200px] lg:w-[336px]"
-    :model-value="props.searchValue"
-    @update:model-value="emit('update:searchValue', $event.toString())"
-  />
+  <div class="relative">
+    <Search class="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+    <Input
+      id="search-input"
+      type="text"
+      placeholder="Enter a book title or author..."
+      class="w-full rounded-lg bg-background pl-8"
+      :model-value="props.searchValue"
+      @update:model-value="emit('update:searchValue', $event.toString())"
+    />
+  </div>
 </template>
