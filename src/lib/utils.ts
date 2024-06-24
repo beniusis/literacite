e838823452ed9a54e99d3ba41008221e07cc00d0
beniusis <rokasbeniu@gmail.com>
2024-06-24 10:26:39 +0300

    chore: remove unused function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,18 +5,6 @@ export const cn = (...inputs: ClassValue[]) => {
   return twMerge(clsx(inputs));
 };
 
-export const createRandomStringID = () => {
-  const ID_LENGTH = 12;
-  const CHARS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-';
-  let randomStringID = '';
-
-  for (let i = 0; i < ID_LENGTH; i++) {
-    randomStringID += CHARS.charAt(Math.floor(Math.random() * CHARS.length));
-  }
-
-  return randomStringID;
-};
-
 export const getCurrentDate = () => {
   return new Date().toISOString().slice(0, 10);
 };
