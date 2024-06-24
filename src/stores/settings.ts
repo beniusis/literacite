042a82f12473307ee71503bcef0d7762a80beabe
beniusis <rokasbeniu@gmail.com>
2024-06-24 17:00:41 +0300

    chore: set default pages per day as 25

--- src/stores/settings.ts
+++ src/stores/settings.ts
@@ -2,10 +2,14 @@ import { defineStore } from 'pinia';
 import { ref } from 'vue';
 import { useToastStore } from './toast';
 
+const DEFAULT_PAGES_PER_DAY = 25;
+
 export const useSettingsStore = defineStore('settings', () => {
   const { success } = useToastStore();
 
-  const pagesPerDay = ref<number>(Number(localStorage.getItem('pages-per-day')) || 1);
+  const pagesPerDay = ref<number>(
+    Number(localStorage.getItem('pages-per-day')) || DEFAULT_PAGES_PER_DAY
+  );
 
   const setPagesPerDay = (value: number) => {
     pagesPerDay.value = value;
